225b1753107931bde96201ed291bcc996dffdc09
Damian Krzeminski <pirxpilot@code42day.com>
2014-04-15 13:05:03 -0400

    use component/event for event registration

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
+all: lint build
+
 lint:
 	jshint index.js
 
--- index.js
+++ index.js
@@ -5,40 +5,49 @@
  *
  */
 
-function contentLoaded(fn) {
+var events = require('event');
 
-	var done = false,
-		top = true,
-		win = window,
-		doc = document,
-		root = doc.documentElement,
+module.exports = domready;
 
-	add = doc.addEventListener ? 'addEventListener' : 'attachEvent',
-	rem = doc.addEventListener ? 'removeEventListener' : 'detachEvent',
-	pre = doc.addEventListener ? '' : 'on',
 
-	init = function(e) {
-		if (e.type == 'readystatechange' && doc.readyState != 'complete') return;
-		(e.type == 'load' ? win : doc)[rem](pre + e.type, init, false);
-		if (!done && (done = true)) fn.call(win, e.type || e);
-	},
-
-	poll = function() {
-		try { root.doScroll('left'); } catch(e) { setTimeout(poll, 50); return; }
-		init('poll');
-	};
-
-	if (doc.readyState == 'complete') fn.call(win, 'lazy');
-	else {
-		if (doc.createEventObject && root.doScroll) {
-			try { top = !win.frameElement; } catch(e) { }
-			if (top) poll();
-		}
-		doc[add](pre + 'DOMContentLoaded', init, false);
-		doc[add](pre + 'readystatechange', init, false);
-		win[add](pre + 'load', init, false);
-	}
-
+function bind(init) {
+	events.bind(document, 'DOMContentLoaded', init);
+	events.bind(document, 'readystatechange', init);
+	events.bind(window, 'load', init);
 }
 
-module.exports = contentLoaded;
+function unbind(init) {
+	events.unbind(document, 'DOMContentLoaded', init);
+	events.unbind(document, 'readystatechange', init);
+	events.unbind(window, 'load', init);
+}
+
+function domready(fn) {
+	var done = false,
+		top = true,
+		root = document.documentElement;
+
+	function init(e) {
+		if (e.type == 'readystatechange' && document.readyState != 'complete') return;
+		unbind(init);
+		if (!done) {
+			done = true;
+			fn(e.type || e);
+		}
+	}
+
+	function poll() {
+		try { root.doScroll('left'); } catch(e) { setTimeout(poll, 50); return; }
+		init('poll');
+	}
+
+	if (done || document.readyState == 'complete') {
+		return fn('lazy');
+	}
+
+	if (document.createEventObject && root.doScroll) {
+		try { top = !window.frameElement; } catch(e) { }
+		if (top) poll();
+	}
+	bind(init);
+}
